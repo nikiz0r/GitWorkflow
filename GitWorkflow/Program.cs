f8be6666ee1916c9e464a72d83ce82152f25325b
Rumenigue Nogueira da Silva <rumenigue.silva@brasilct.com>
2017-09-05 09:46:36 -0300

    Add project files.

---
+++ GitWorkflow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitWorkflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
